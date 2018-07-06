80e003110a02393f5f9358eae2281bc33c04d39e
hoangvu2511 <map1473@gmail.com>
2018-07-06 09:54:51 +0700

    resize sign up layout

--- app/src/main/java/vinova/intern/best_trip/signIn/SignInActivity.kt
+++ app/src/main/java/vinova/intern/best_trip/signIn/SignInActivity.kt
@@ -11,4 +11,8 @@ class SignInActivity : Fragment() {
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 		return inflater.inflate(R.layout.sign_in_fragment,container,false)
 	}
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+	}
 }
